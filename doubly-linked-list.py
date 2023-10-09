7286d950372b47813a7c8bf69a937c64ba4726c1
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2023-10-09 21:27:44 +0900

    finish doubly linked list exercise.

--- doubly-linked-list.py
+++ doubly-linked-list.py
@@ -7,5 +7,33 @@ class Node:
 
 class DoublyLinkedList:
     def __init__(self) -> None:
-        pass
+        self.head = None
+        self.tail = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
+    
+    def print_reverse(self):
+        if not self.tail:
+            print("No values within this list")
+        else:
+            current_node = self.tail
+            while current_node != None:
+                print(current_node.data)
+                current_node = current_node.prev
+
+dllist = DoublyLinkedList()
+dllist.append(12)
+dllist.append(42)
+dllist.append(52)
+dllist.append(10)
+
+dllist.print_reverse()
